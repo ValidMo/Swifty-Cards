1f72b2298314e031886492423f9092e9aba1976e
ValidMo <mohammadivalid@gmail.com>
2023-09-19 12:43:57 +0330

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by Valid Mohammadi on 19.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
