4f5a7d8a4e669275c780b9c660b1c1d793ef3ee5
Valid Mohammadi <63536756+ValidMo@users.noreply.github.com>
2024-04-17 13:31:12 +0330

    UI changes in AboutUsView

--- Cards/Views/AboutUsView.swift
+++ Cards/Views/AboutUsView.swift
@@ -12,7 +12,23 @@ struct AboutUsView: View {
     @Environment(\.presentationMode) var presentationMode
     
     var body: some View {
-        Text("Hi my name is Noyan, you can visit my website https://noyan.me ")
+        VStack(alignment: .center){
+            Text("Visit my website")
+                .font(.custom("Aldrich-Regular", size: 20))
+                .padding()
+               
+        Text("https://noyan.me ")
+                .customText()
+                .frame(width: 340, height: 78)
+                
+                .background(
+                RoundedRectangle(cornerRadius: 24)
+                    .fill(Color("TextField"))
+                    .shadow(color: Color.gray.opacity(0.4), radius: 10, x: 5, y: 5)
+                )
+        }
+        
+        
     }
 }
 
